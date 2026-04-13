4aa8682af4f6b961306f7d982a3d868e7eb5b3bd
joser <jose.reyesfabian@gmail.com>
2026-04-13 08:16:23 -0400

    41326

---
+++ src/Section3/Methods.java
@@ -0,0 +1,18 @@
+package Section3;
+
+public class Methods {
+
+  static void main() {
+
+    System.out.println(add(5,4));
+
+  }
+
+
+  public static int add(int x, int y){
+    return x + y;
+  }
+  public static double add(double x, double y){
+    return x + y;
+  }
+}
